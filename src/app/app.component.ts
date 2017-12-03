f547326639acfde6caa49b6cbe744d540e5a9e0b
Golyak <Golyak@kai24.com>
2017-12-03 22:23:24 +0200

    directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Создание директивы !!!';
+  title2 = 'WebForMySelf';
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  obj = {
+    first_name: 'Dima',
+    last_name: 'Holiak',
+    nested: {
+      birthday: new Date(),
+      eyesColor: 'brown'
+    }
+  };
+
+
+  items = [1, 2, 3, 4, 5];
+  current = 1;
+
+  onClick(item: number) {
+   this.current = item;
+  }
 }
