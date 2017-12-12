696dd1ae3e1edcd849b705777e1bfa20ed5b04b6
Bumbilo <dimagolyak@gmail.com>
2017-12-12 12:24:11 +0200

    testing component

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+
+  isCarVisible = true;
+  title = 'My car header';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
